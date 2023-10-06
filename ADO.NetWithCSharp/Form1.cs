3e7133a9b2d1cc95b5a9988a189914ec41844d09
Parham darvishi <darvishiparham14@gmail.com>
2023-10-06 17:00:57 +0330

    Add project files.

---
+++ ADO.NetWithCSharp/Form1.cs
@@ -0,0 +1,10 @@
+namespace ADO.NetWithCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ADO.NetWithCSharp/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+using ADO;
+
+namespace ADO.NetWithCSharp.Models;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+}
